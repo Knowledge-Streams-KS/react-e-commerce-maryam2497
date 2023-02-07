8fceaa8695b9c912e3ead2c0472f6efcfdb6eb3d
maryam2497 <maryamrizvi2497@gmail.com>
2023-02-07 23:42:16 +0500

    searching filter implemented

--- src/Components/list/list.jsx
+++ src/Components/list/list.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '../card/card';
 import './list.css'
 
 // received products as props from product.jsx
-const List = ({ products }) => (
+const List = ({ products }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value.toLowerCase());
+  };
+
+  const filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(searchTerm);
+  });
+
+  return (
     <div className="list-container">
-        
-    {/* mapping the products,sending products to card.jsx as props */}
-    {products.map(p => (
-      <Card
-        key={p.id}
-        title={p.title}
-        price={p.price}
-        category={p.category}
-        description={p.description}
-        image={p.image}
-      />
-    ))}
-  </div>
-);
+      <div className="search-container">
+        <input type="text" value={searchTerm} onChange={handleSearch} />
+        <button>Search</button>
+      </div>
+      {/* mapping the products,sending products to card.jsx as props */}
+      {filteredProducts.map(p => (
+        <Card
+          key={p.id}
+          title={p.title}
+          price={p.price}
+          category={p.category}
+          description={p.description}
+          image={p.image}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default List;
