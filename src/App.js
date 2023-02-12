bd087880c3033f33fe3af1a68f4b049700a47029
maryam2497 <maryamrizvi2497@gmail.com>
2023-02-12 15:30:16 +0500

    categories,product details and error pages created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import axios from 'axios';
 function App() {
   return (
     <div className="App">
-        <Product/>
+       <Routetes>
+        <Route  path='/' element={<NavBar />}>
+        <Route path='home' element={<Home />}></Route>
+          <Route path='/categories' element={<Categories/>}></Route>
+          <Route path='/product' element={<Product/>}></Route>
+          <Route path='/productDetails' element={<ProductDetails/>}></Route>
+          
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/*' element={<Error/>}></Route>
+        </Route></Routetes>
     </div>
   );
 }
