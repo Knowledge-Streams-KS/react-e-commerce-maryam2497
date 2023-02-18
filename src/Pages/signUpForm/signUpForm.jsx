47066e05b0cf1a00c4a29a798d6cb641c723a2fe
maryam2497 <maryamrizvi2497@gmail.com>
2023-02-18 23:25:52 +0500

    styling added to SignUp page

--- src/Pages/signUpForm/signUpForm.jsx
+++ src/Pages/signUpForm/signUpForm.jsx
@@ -3,6 +3,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { Link, useNavigate } from "react-router-dom";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import "./signUpForm.css";
 import app from "../../firebase";
 import { Navigate } from "react-router-dom";
 const SignUpForm = () => {
@@ -11,31 +12,21 @@ const SignUpForm = () => {
   const [password, setPassword] = useState({});
 
   const SignUp = (values) => {
-// const navigate = useNavigate();
 createUserWithEmailAndPassword(auth, values.email, values.password)
   .then((userCredential) => {
-    // Signed in 
     const user = userCredential.user;
     console.log(user)
     alert("Successfully created account")
-    // navigate("/loginForm");
-    // ...
   })
   .catch((error) => {
     const errorCode = error.code;
-    // const errorMessage = error.message;
     alert(errorCode)
-    // ..
   });
   }
 
   const [formData, setFormData] = useState({
     email: "",
-    // firstName: "",
-    // lastName: "",
-    // phoneNumber: "",
     password: "",
-    // passwordConfirmation: "",
   });
 
   const handleSubmit = (values) => {
@@ -49,65 +40,39 @@ createUserWithEmailAndPassword(auth, values.email, values.password)
     email: Yup.string()
       .email("Invalid email address")
       .required("Email is required"),
-      // firstName: Yup.string().required("First name is required"),
-      // lastName: Yup.string().required("Last name is required"),
-      // phoneNumber: Yup.string()
-      // .matches(
-      //   /^((\+92)?(0092)?(92)?(0)?)(3)([0-9]{9})$/,
-      //   "Invalid phone number format"
-      // )
-      // .required("Phone number is required"),
     password: Yup.string()
       .min(6, "Password must be at least 6 characters")
       .required("Password is required"),
-      // passwordConfirmation: Yup.string().label('confirm password').required().oneOf([Yup.ref('password'), null], 'Passwords must match'),
   });
 
   return (
     <Formik
       initialValues={{
         email: "",
-        // firstName: "",
-        // lastName: "",
-        // phoneNumber: "",
         password: "",
-        // passwordConfirmation: "",
       }}
       onSubmit={handleSubmit}
       validationSchema={validationSchema}
     >
       {({ isSubmitting }) => (
-        <Form>
+        <Form className="signup-form">
+          <div className="form-field">
           <Field type="email" name="email" placeholder="Email..." />
           <ErrorMessage name="email" component="div" />
           <br />
-          {/* <Field type="text" name="firstName" placeholder="First Name..." />
-          <ErrorMessage name="firstName" component="div" />
-          <br />
-          <Field type="text" name="lastName" placeholder="Last Name..." />
-          <ErrorMessage name="lastName" component="div" />
-          <br />
-          <Field type="text" name="phoneNumber" placeholder="Phone Number..." />
-          <ErrorMessage name="phoneNumber" component="div" />
-          <br /> */}
-          <Field type="password" name="password" placeholder="Password..." />
+          </div>
+          <div className="form-field">
+          <Field  type="password" name="password" placeholder="Password..." />
           <ErrorMessage name="password" component="div" />
-          <br />
-          {/* <Field
-            type="password"
-            name="passwordConfirmation"
-            placeholder="Comfirm Password..."
-          /> */}
-          {/* <ErrorMessage name="passwordConfirmation" component="div" />
-          <br /> */}
-          {/* <Link to="/home"> */}
-          <button type="submit" disabled={isSubmitting}>
+          <br /></div>
+          <button type="submit" disabled={isSubmitting} className="submit-button">
             Submit
           </button>
-          {/* </Link> */}
+          <Link to="/loginForm" className="already-user-link">Already a User?</Link>
         </Form>
       )}
     </Formik>
+  
   );
 };
 
