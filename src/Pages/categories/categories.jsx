e32c58f9afdc85830bb483189ce95a9bff612ba4
maryam2497 <maryamrizvi2497@gmail.com>
2023-02-18 22:33:27 +0500

    styling added to categories page

--- src/Pages/categories/categories.jsx
+++ src/Pages/categories/categories.jsx
@@ -2,29 +2,29 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link, useParams } from "react-router-dom";
 import Product from '../product/product';
+import './categories.css';
+
 const Categories = () => {
-const [categories, setCategories] = useState([]);
-const { category } = useParams();
+  const [categories, setCategories] = useState([]);
+  const { category } = useParams();
 
-useEffect(() => {
-axios.get('https://fakestoreapi.com/products/categories')
-.then(response => {
-setCategories(response.data);
-console.log(response.data);
-});
-}, []);
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products/categories')
+      .then(response => {
+        setCategories(response.data);
+        console.log(response.data);
+      });
+  }, []);
 
-return (
-  <div>
-    <br/>
-    <br/>
-    {categories.map((category,index) => (
-     
-      <Link key={index} to={`/product/${category}`} >
-        {category}<br/>
-      </Link >
-    ))}
-  </div>
-);
+  return (
+    <div className="categories-container">
+      {categories.map((category, index) => (
+        <Link key={index} to={`/product/${category}`} >
+         <button className="categories-btn">{category}</button>
+        </Link>
+      ))}
+    </div>
+  );
 };
+
 export default Categories;
